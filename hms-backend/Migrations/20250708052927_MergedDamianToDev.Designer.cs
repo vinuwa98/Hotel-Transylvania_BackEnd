852c73da245e36006ed204de378b7c95ada00cf2
Damian Chamel <58256720+Damian-crypto@users.noreply.github.com>
2025-07-08 11:02:57 +0530

    Added new migrations for merge

--- hms-backend/Migrations/20250708052927_MergedDamianToDev.Designer.cs
+++ hms-backend/Migrations/20250708052927_MergedDamianToDev.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace hms_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250707085028_UncommentedIgnoringSecurityStampFromTheIdentityUser")]
-    partial class UncommentedIgnoringSecurityStampFromTheIdentityUser
+    [Migration("20250708052927_MergedDamianToDev")]
+    partial class MergedDamianToDev
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
